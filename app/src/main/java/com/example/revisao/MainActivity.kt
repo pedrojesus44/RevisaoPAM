2519f718858f7bf77599b396d23d42443ac17899
pedrojesus44 <pedro.jesus44@etec.sp.gov.br>
2022-08-01 11:04:32 -0300

    bah1

--- app/src/main/java/com/example/revisao/MainActivity.kt
+++ app/src/main/java/com/example/revisao/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.revisao
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -20,7 +21,9 @@ class MainActivity : AppCompatActivity() {
         val btnCadastrar:Button = findViewById(R.id.btnCadastrar)
 
         btnCadastrar.setOnClickListener {
-            Toast.makeText(this, "Nome: " + txtNome.text.toString(), Toast.LENGTH_LONG).show()
+            //Toast.makeText(this, "Nome: " + txtNome.text.toString(), Toast.LENGTH_LONG).show()
+            val intent = Intent(this, Resultado::class.java)
+            startActivity(intent)
         }
 
     }
--- app/src/main/java/com/example/revisao/Resultado.kt
+++ app/src/main/java/com/example/revisao/Resultado.kt
@@ -1,11 +1,21 @@
 package com.example.revisao
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Resultado : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado)
+
+        val btnVoltar: Button = findViewById(R.id.btnVoltar)
+
+        btnVoltar.setOnClickListener {
+            //Toast.makeText(this, "Nome: " + txtNome.text.toString(), Toast.LENGTH_LONG).show()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
